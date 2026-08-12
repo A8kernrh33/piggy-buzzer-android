08b5f196ce0e74edf888cbaa5a072e0fc901d531
A8kernrh33 <ankitminecrafta8kernrh@gmail.com>
2026-08-12 06:02:05 +0530

    Add Android app Gradle configuration

---
+++ app/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.a8kernrh33.buzzer"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.a8kernrh33.buzzer"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:34.17.0"))
+    implementation("com.google.firebase:firebase-messaging")
+}
